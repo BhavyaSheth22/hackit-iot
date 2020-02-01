8f78b947e7adc31d8bfa7facdc7dbccd2b9f39b1
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-02-01 17:30:36 +0530

    fixed isseue where data was not being recieved'

--- threading_test.py
+++ threading_test.py
@@ -5,28 +5,29 @@ import time
 import paho.mqtt.client as paho
 import random
 
-thread_local = threading.local()
 
-def get_client():
+def connect_client():
     id_ = "client"+str(random.randint(0,10000000))
     print(f"inside get id: {id_}")
-    if not hasattr(thread_local, "client"):
-        thread_local.client = paho.Client(id_)
-    return thread_local.client, id_
 
-def connect_client(a):
-    client_, id__ = get_client()
+    client_ = paho.Client(id_)
     client_.connect("127.0.0.1",1883)
     
     for i in range(3):
-        print(client_.publish("house", id__ + ": " + str(i)).is_published())
-
+        print(client_.publish("house", id_ + ": " + str(i)).is_published())
+        time.sleep(0.5)
+        
     client_.disconnect()
     
 def connect_all_clients(number):
     print("inside connect all client")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(connect_client,[i for i in range(number)])
+
+    thread_list = list()
+
+    for _ in range(number):
+        t = threading.Thread(target=connect_client)
+        thread_list.append(t)
+        t.start()
 
 if __name__ == "__main__":
     number = int(input("enter pub: "))
