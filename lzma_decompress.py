dec0b122449ea1defeeb019c1a933ebd442a057a
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2020-02-02 05:34:05 +0530

    deleted files

--- lzma_decompress.py
+++
@@ -1,7 +0,0 @@
-import lzma
-
-original_data = open('comp_block.txt', 'rb').read()
-
-og_block = lzma.decompress(original_data)
-with open("og_data.txt", 'wb') as f:
-    f.write(og_block)
--- subscriber.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.subscribe as subscribe
-
-def print_msg(client, userdata, message):
-    print("%s : %s" % (message.topic, message.payload))
-
-subscribe.callback(print_msg, "house", hostname="127.0.0.1")
